2d999abb85bc479adb4fb7ff303ee1e8baf6647e
jakob-ra <49194118+jakob-ra@users.noreply.github.com>
2020-01-16 13:16:36 +0100

    R-code for HW2

---
+++ HW_2/SML_hw2_code.R
@@ -0,0 +1,10 @@
+# *------------------------------------------------------------------
+# | PROGRAM NAME: Supervised Machine Learning - HW 2
+# | DATE: 16-1-2020
+# | CREATED BY: Jakob Rauch
+# *----------------------------------------------------------------
+
+# Load data
+githubURL <- "https://github.com/jakob-ra/Supervised-Machine-Learning/raw/master/HW_2/supermarket1996.RData"
+load(url(githubURL))
+
