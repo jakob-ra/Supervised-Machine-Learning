31972bf42b04d61b031c5660dd089b8319af37be
kosommer <59961518+kosommer@users.noreply.github.com>
2020-01-16 19:14:04 +0100

    Add files via upload
    added the k-fold, outstanding problems:
    K-fold grid rounding, so far we dont use all the data
    results for rmse vary to much when looking at the plots

--- HW_2/SML_hw2_code.R
+++ HW_2/SML_hw2_code.R
@@ -63,7 +63,7 @@ loss = function(y,X,alpha,lambda,beta,epsilon){
 }
 
 # MM-algorithm
-elastic_net_MM = function(y,X,alpha,lambda,beta_0,epsilon=10^(-8)){
+elastic_net_MM = function(y,X,alpha,lambda,beta_0=rep(0,p),epsilon=10^(-8)){
   # Fits an elastic net model via MM-algorithm. Returns vector beta_hat 
   # for given data ,hyperparameters lambda and alpha, intitial parameter guess beta_0, and convergence threshold epsilon
   n = length(y) # Number of observations
@@ -80,38 +80,51 @@ elastic_net_MM = function(y,X,alpha,lambda,beta_0,epsilon=10^(-8)){
     beta = solve(A, 1/n*t(X)%*%y)
     l_new = loss(y,X,alpha,lambda,beta,epsilon)[[1]]
     
-    print(k)
+   # print(k)
     k = k + 1
-    print(l_old-l_new)
+    #print(l_old-l_new)
   }
   
   return(beta)
 }
 
-p = length(X[1,]) # Number of parameters
-beta_0 = rep(0,p) # Initialize with beta_0 a vetor of 0s
+# p = length(X[1,]) # Number of parameters
+# beta_0 = rep(0,p) # Initialize with beta_0 a vetor of 0s
+# 
+# alpha = 0.5
+# lambda = 0.05428675
+# elastic_net_MM(y,X,alpha,lambda,beta_0)
 
-alpha = 0.5
-lambda = 0.05428675
-elastic_net_MM(y,X,alpha,lambda,beta_0)
 
-# Function for k-fold crossvalidation
-elastic_net_MM = function(y,X,k){
-  # Tunes hyperparameters alpha and lambda via k-fold crossvalidation. Returns optimal alpha and lambda
-  lambda_values = 10^seq(-2, 5, length.out = 50)
-  alpha_values = seq(0, 1, length.out = 10)
-  
-  n = length(y) # Number of observations
-  reshuffled_indices = sample(seq(1,n,1), n, replace=FALSE) # Shuffle indices
-  n_test = round(n/k) # if n=77 and k=10 we will have 8 obs. in the test set 
-  n_train = n-n_test
-  for (i in seq(1,k-1,1)){
-    # Divide data into test and training
-    y_test = y[c(reshuffled_indices[(1+(i-1)*n_test):(i*n_test)])]
-    y_train = y[-c(reshuffled_indices[(1+(i-1)*n_test):(i*n_test)])]
-    X_test = X[c(reshuffled_indices[(1+(i-1)*n_test):(i*n_test)]),]
-    X_train = X[-c(reshuffled_indices[(1+(i-1)*n_test):(i*n_test)]),]
-    # 
+k_fold = function(y,X,k,alpha,lambda){
+  # Function for k-fold crossvalidation, returns rmse for given alpha and lambda
+n = length(y) # Number of observations
+p = (dim(X)[2])
+reshuffled_indices = sample(seq(1,n,1), n, replace=FALSE) # Shuffle indices
+n_test = round(n/k) # if n=77 and k=10 we will have 8 obs. in the test set 
+n_train = n-n_test
+MSE=array(0,k-1)
+for (i in seq(1,k-1,1)){
+  # Divide data into test and training
+  y_test = y[c(reshuffled_indices[(1+(i-1)*n_test):(i*n_test)])]
+  y_train = y[-c(reshuffled_indices[(1+(i-1)*n_test):(i*n_test)])]
+  X_test = X[c(reshuffled_indices[(1+(i-1)*n_test):(i*n_test)]),]
+  X_train = X[-c(reshuffled_indices[(1+(i-1)*n_test):(i*n_test)]),]
+  beta=elastic_net_MM(y_train,X_train,alpha,lambda,beta_0=rep(0,p)) ##get beta estimates from MM algorithm
+  fitted_val=(X_test)%*%beta ##get y_hat
+  MSE[i]=1/n_test*t(y_test-fitted_val)%*%(y_test-fitted_val) ##get MSE values
+}
+rmse=sqrt(sum(MSE)/(k-1))
+return(rmse)
+}
+
+# Tunes hyperparameters alpha and lambda via k-fold crossvalidation. Returns optimal alpha and lambda
+lambda_values = 10^seq(-2, 5, length.out = 50)
+alpha_values = seq(0, 1, length.out = 10)
+rmse=matrix(0,length(alpha_values),length(lambda_values)) ## create matrix, which will be filled with rmse values per hyperparamter combination
+for (i in 1:length(alpha_values)){
+  for (j in 1:length(lambda_values)){
+    rmse[i,j]=k_fold(y,X,10,alpha_values[i],lambda_values[j]) ##Fill the matrix with rmse estimates
   }
-
-  }
+}
+plot(rmse[5,])
