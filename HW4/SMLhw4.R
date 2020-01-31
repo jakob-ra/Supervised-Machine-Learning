08294619653097b48da2606b2f06c74bd5d4a619
kosommer <59961518+kosommer@users.noreply.github.com>
2020-01-31 14:36:58 +0100

    Add files via upload
    k-fold and optimal lambda code added, MM algorithm fixed, comparison with package added. optimal lambda different, probably uses different loss

--- HW4/SMLhw4.R
+++ HW4/SMLhw4.R
@@ -20,7 +20,7 @@ v.update = function(y,X,vold,lambdaP){
   # returns updated v+
   qtilde = X%*%vold
   b = qtilde*((qtilde<=-1)&(y==-1)) - ((qtilde>-1)&(y==-1)) + 
-    qtilde*((qtilde<=1)&(y==1)) + ((qtilde>1)&(y==1))
+    ((qtilde<=1)&(y==1)) + qtilde*((qtilde>1)&(y==1))
   # A = diag(1,nrow(X)) # skip for computational simplicity
   vnew = solve(t(X)%*%X + lambdaP)%*%t(X)%*%b
   
@@ -29,7 +29,7 @@ v.update = function(y,X,vold,lambdaP){
 
 ## SVMMaj MM algorithm
 
-SVMMaj = function(y,X,lambda,v0,epsilon = 1e-5){
+SVMMaj_manual = function(y,X,lambda,v0,epsilon = 1e-5){
   # returns optimal v = (c,w)' given data y, X, penalty lambda
   # by running the MM algorithm with initial level v0 and stopping criterion
   # epsilon
@@ -38,7 +38,7 @@ SVMMaj = function(y,X,lambda,v0,epsilon = 1e-5){
   lambdaP[1,1] = 0
   
   Lold = loss.svm.quad(y,q = X%*%v0,w = v0[-1],lambda)
-  cat('L0 =',Lold,"\n")
+  #cat('L0 =',Lold,"\n")
   k = 1
   Lnew = Lold
   v = v0
@@ -48,13 +48,61 @@ SVMMaj = function(y,X,lambda,v0,epsilon = 1e-5){
     vnew = v.update(y,X,v,lambdaP)
     Lnew = loss.svm.quad(y,q = X%*%vnew,w = vnew[-1],lambda)
     v = vnew
-    cat('after iteration ',k," L = ",Lnew,"\n")
+    #cat('after iteration ',k," L = ",Lnew,"\n")
   }
-  cat("stopping crit achieved\n")
+  #cat("stopping crit achieved\n")
   return(vnew)
 }
+### k-fold
+k_fold_crossval = function(y,X,k,lambda,v0){
+  # Function for k-fold crossvalidation, returns RMSE for given alpha and lambda
+  n = length(y) # Number of observations
+  p = (dim(X)[2]) # Number of parameters
+  
+  # Reshuffle data and split into k groups of equal size
+  reshuffled_indices = sample(seq(1,n,1), n, replace=FALSE) # Shuffle indices of our n observations
+  n_test = floor(n/k) # n=77 and k=10 would give 7 obs. in the test set the rest of the observations
+  # in the training set. For n=77, using k=11 or k=7 is advisable to get evenly sized groups.
+  
+  loss=array(0,k) # Vector to hold the loss for each fold
+  
+  # Loop over the k folds and save MSEs
+  for (i in seq(1,k,1)){
+    # Divide data into test and training
+    y_test = y[c(reshuffled_indices[(1+(i-1)*n_test):(i*n_test)])]
+    y_train = y[-c(reshuffled_indices[(1+(i-1)*n_test):(i*n_test)])]
+    X_test = X[c(reshuffled_indices[(1+(i-1)*n_test):(i*n_test)]),]
+    X_train = X[-c(reshuffled_indices[(1+(i-1)*n_test):(i*n_test)]),]
+    
+    beta = SVMMaj(y_train,X_train,lambda,v0) # get beta estimates for training data from majorization algorithm 
+    fitted_val = (X_test)%*%beta # get qhat for test data
+    TP=sum(fitted_val>1&y_test==1)
+    TN=sum(fitted_val<(-1)&y_test==(-1))
+    loss[i]=1-(TP+TN)/n_test # save missclassification error values for test data
+  }
+  loss=mean(loss) ##take average over found losses
+  
+  return(loss)
+}
 
-
+min_loss = function(y,X,k=10,lambda_values=10^seq(-3, 4, length.out = 10),v0){
+  # Tunes hyperparameters lambda via k-fold crossvalidation. Returns optimal lambda
+  # as well as the resulting loss
+  
+  loss = array(0,length(lambda_values)) # create vector to hold loss for each lambda
+  
+  # Loop over hyperparameter combinations
+  for (j in 1:length(lambda_values)){
+    loss[j] = k_fold_crossval(y,X,k,lambda_values[j],v0) # Fill the matrix with losses
+  }
+  
+  
+  min_index = which.min(loss) # Find index of lowest loss
+  lambda_min = lambda_values[min_index] # lambda value at lowest loss
+  loss_min = loss[min_index] # lowest misclassification error
+  
+  return(list(loss,lambda_min,loss_min))
+}
 ### main
 
 # load data
@@ -81,11 +129,22 @@ lambda = 0.01
 v0 = rep(0,p+1)
 
 # lets go
-vhat = SVMMaj(y,X,lambda,v0,epsilon = 1e-5)
+vhat = SVMMaj_manual(y,X,lambda,v0,epsilon = 1e-5)
 vhat
 
+#mis_error=k_fold_crossval(y,X,10,lambda=0.1,v0)
+man_results=min_loss(y,X,10,lambda_values=seq(0.01, 1, length.out = 20),v0=v0) ### find optimal lambda
+loss_vector=man_results[[1]]
+loss_vector
+opt_lambda=man_results[[2]]
+plot(loss_vector,type="l")
+vhat=SVMMaj_manual(y,X,opt_lambda,v0,epsilon = 1e-5) ##our solution with optimal lambda
 
 
+##Compare with package
+#library(SVMMaj)
 
-
-
+pack_results=svmmajcrossval(X,y,ngroup=10,search.grid = list(lambda=seq(0.01, 1, length.out = 20))) ##Find optimal lambda
+cbind(opt_lambda,pack_results$param.opt)
+pack_vhat=svmmaj(X,y,opt_lambda,hinge="quadratic",scale="none") ###get coefficients for optimal beta
+cbind(vhat,pack_vhat$beta) ## compare our solution to package
