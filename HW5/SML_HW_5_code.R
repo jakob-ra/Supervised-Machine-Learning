4ba6a0efdd2ae6f04193bab117473685c81cf0c9
mnordfeldt <55142893+mnordfeldt@users.noreply.github.com>
2020-02-07 11:22:28 +0100

    test

---
+++ HW5/SML_HW_5_code.R
@@ -0,0 +1,8 @@
+# TEST #
+# Supervised Machine Learning - HW5 - #
+# Maja Nordfeldt, Feb 2020 #
+
+# ------------------ Data Preparation --------------------#
+data_url <- "https://github.com/jakob-ra/Supervised-Machine-Learning/blob/master/HW5/imdb.RData"
+load(url(data_url))
+
